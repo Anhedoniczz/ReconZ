919ef89196dd20807d1f9107aeeb1cb1320f74bf
Anhedonic <93049946+Anhedoniczz@users.noreply.github.com>
2024-05-15 21:46:42 +0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -14,6 +14,7 @@ cd ~/tools
 git clone https://github.com/gotr00t0day/spyhunt
 pip3 install -r ~/tools/spyhunt/requirements.txt
 sudo python3 spyhunt/install.py
+sudo rm -rf ~/tools/spyhunt/spyhunt.py
 wget -P ~/tools/spyhunt https://raw.githubusercontent.com/Anhedoniczz/ReconZ/main/Resources/spyhunt.py
 git clone https://github.com/s0md3v/XSStrike
 pip3 install -r ~/tools/XSStrike/requirements.txt
